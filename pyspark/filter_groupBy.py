99bf3ac5063964cb8f62d3d001f7026c75c58694
glommmer <glommer@kakao.com>
2025-03-21 17:23:34 +0900

    Add note about groupBy & Rename ast

--- pyspark/filter_groupBy.py
+++ pyspark/filter_groupBy.py
@@ -5,20 +5,25 @@ from pyspark.sql.functions import year, col
 spark = SparkSession.builder.getOrCreate()
 people_df = spark.read.parquet("/FileStore/sample_data/userdata1.parquet")
 
-people_df.filter(people_df['gender'] == 'F').count()
-people_df.filter(people_df.gender == 'F').count()
+people_df.filter(people_df["gender"] == "F").count()
+people_df.filter(people_df.gender == "F").count()
 people_df.filter("gender == 'F'").count()
-people_df.filter(year("birthDate") > '1955').count()
+people_df.filter(year("birthDate") > "1955").count()
 people_df.select("firstName").filter("gender='M'" and year("birthDate") > "1960")
-people_df.select("firstName", year("birthDate")).filter("gender='M'" and year("birthDate") > "1960")
+people_df.select("firstName", year("birthDate")).filter(
+    "gender='M'" and year("birthDate") > "1960"
+)
 
 people_df.groupBy("gender").count().show()
 people_df.orderBy("firstName").groupBy("firstName").count().show(5)
-people_df.groupBy("firstName").count().orderBy("count", ascending=False).show(5)  # count()로 sort
+people_df.groupBy("firstName").count().orderBy("count", ascending=False).show(
+    5
+)  # count()로 sort
 people_df.groupBy("firstName").count().orderBy("count").show(5)
 
-people_df.groupBy("firstName").count().filter(col("count") > "5430") \
-         .orderBy("count", ascending=False).show(6)
+people_df.groupBy("firstName").count().filter(col("count") > "5430").orderBy(
+    "count", ascending=False
+).show(6)
 
 # `count()`를 적용하면 count라는 column이 생성되는데, 생성된 count column에 접근하기 위해서는 `col()` 함수를 import하고 적용해야 함
 # `col()`에 대한 추가 설명
@@ -27,3 +32,13 @@ people_df.groupBy("firstName").count().filter(col("count") > "5430") \
 #        > other_col이라는 컬럼을 반환하려면 `col("other_col")`이라고 써야 함
 #    - 함수가 입력자로 column 자체를 요구하는 때가 있기도 하고
 #    - string이 아닌 column을 쓰고자 할 때 `col()`을 써야 함
+
+dordon_df = (
+    people_df.select(year("birth_date").alias("birth_year"), "first_name")
+    .filter((col("first_name") == "Donna") | (col("first_name") == "Dorothy"))  # or
+    .filter("gender == 'F'")
+    .orderBy("birth_year")
+    .groupBy("birth_year", "first_name")  # Multiple Group By
+    .count()
+    .show(5)
+)
