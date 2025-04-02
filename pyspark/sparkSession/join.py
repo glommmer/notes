932de5fe6dd4807fcce32f00c843c4f82556d661
glommmer <glommer@kakao.com>
2025-04-02 15:58:54 +0900

    Edit note about join

--- pyspark/sparkSession/join.py
+++ pyspark/sparkSession/join.py
@@ -29,3 +29,102 @@ inner_join = ta.join(tb, ["name"])
 # column명이 같다면 이렇게도 할 수는 있으나
 # 권장되는 방법은 아님
 ￼
+# Inner Join
+inner_join = ta.join(tb, ta.name == tb.name)
+''' 
++------+---+------+---+
+|  name| id|  name| id|
++------+---+------+---+
+| Ninja|  3| Ninja|  3|
+|Pirate|  1|Pirate|  2|
++------+---+------+---+ 
+'''
+inner_join = ta.join(tb, on = "name")
+inner_join = ta.join(tb, ["name"])
+''' 
++------+---+---+
+|  name| id| id|
++------+---+---+
+| Ninja|  3|  3|
+|Pirate|  1|  2|
++------+---+---+ 
+'''
+
+# Left (Outer) Join
+left_join = ta.join(tb, ta.name == tb.name, how = 'left')
+''' 
++---------+---+------+----+
+|     name| id|  name|  id|
++---------+---+------+----+
+|   Monkey|  2|  null|null|
+|    Ninja|  3| Ninja|   3|
+|   Pirate|  1|Pirate|   2|
+|Spaghetti|  4|  null|null|
++---------+---+------+----+ 
+'''
+left_join = ta.join(tb, on = "name", how = 'left')
+left_join = ta.join(tb, ["name"], how = 'left')
+''' 
++---------+---+----+
+|     name| id|  id|
++---------+---+----+
+|   Monkey|  2|null|
+|    Ninja|  3|   3|
+|   Pirate|  1|   2|
+|Spaghetti|  4|null|
++---------+---+----+ 
+'''
+
+# Right (Outer) Join
+right_join = ta.join(tb, ta.name == tb.name, how = 'right')
+''' 
++------+----+-----------+---+
+|  name|  id|       name| id|
++------+----+-----------+---+
+|  null|null|Darth Vader|  4|
+| Ninja|   3|      Ninja|  3|
+|Pirate|   1|     Pirate|  2|
+|  null|null|   Rutabaga|  1|
++------+----+-----------+---+ 
+'''
+right_join = ta.join(tb, on = "name", how = 'right')
+right_join = ta.join(tb, ["name"], how = 'right')
+''' 
++-----------+----+---+
+|       name|  id| id|
++-----------+----+---+
+|Darth Vader|null|  4|
+|      Ninja|   3|  3|
+|     Pirate|   1|  2|
+|   Rutabaga|null|  1|
++-----------+----+---+ 
+'''
+
+# Full Outer Join
+full_outer_join = ta.join(tb, ta.name == tb.name, how = 'full')
+'''	
++---------+----+-----------+----+
+|     name|  id|       name|  id|
++---------+----+-----------+----+
+|     null|null|Darth Vader|   4|
+|   Monkey|   2|       null|null|
+|    Ninja|   3|      Ninja|   3|
+|   Pirate|   1|     Pirate|   2|
+|     null|null|   Rutabaga|   1|
+|Spaghetti|   4|       null|null|
++---------+----+-----------+----+ 
+'''
+full_outer_join = ta.join(tb, on = "name", how = 'full')
+full_outer_join = ta.join(tb, ["name"], how = 'full')
+'''	
++-----------+----+----+
+|       name|  id|  id|
++-----------+----+----+
+|Darth Vader|null|   4|
+|     Monkey|   2|null|
+|      Ninja|   3|   3|
+|     Pirate|   1|   2|
+|   Rutabaga|null|   1|
+|  Spaghetti|   4|null|
++-----------+----+----+ 
+'''
